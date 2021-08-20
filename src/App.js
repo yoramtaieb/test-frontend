803125cbf13d6ef3d9cfefa7044ed3cda7375797
yoramtaieb <yoram.hob@outlook.fr>
2021-08-21 00:02:08 +0200

    :fire: first test done :fire:

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
+import React, { useState } from "react";
+
 function App() {
-  return <></>;
+  let initialState = { firstName: "", lastName: "", email: "", password: "" };
+
+  let [state, setState] = useState(initialState);
+
+  const handleChange = (e) => {
+    let { name, value } = e.target;
+    setState((prevState) => ({ ...prevState, [name]: value }));
+    console.log(state);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form onSubmit={handleSubmit} data-testid="form_login">
+      <div className="container_firstName">
+        <label aria-label="firstName" htmlFor="firstName">
+          Prénom
+        </label>
+        <input type="text" name="firstName" onChange={handleChange} required />
+      </div>
+      <div className="container_lastName">
+        <label aria-label="lastName" htmlFor="lastName">
+          Nom
+        </label>
+        <input type="text" name="lastName" onChange={handleChange} required />
+      </div>
+      <div className="container_email">
+        <label aria-label="email" htmlFor="email">
+          Email
+        </label>
+        <input type="email" name="email" onChange={handleChange} required />
+      </div>
+      <div className="container_mdp">
+        <label aria-label="password" htmlFor="password">
+          Mot de passe
+        </label>
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          required
+        />
+      </div>
+      <button>Envoyer</button>
+    </form>
+  );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable jest/valid-expect */
+import App from "./App";
+import { render, fireEvent, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("formLogin", () => {
+  it("should show form login", () => {
+    render(<App />);
+    const components = screen.getByTestId("form_login");
+    expect(components).toBeInTheDocument();
+  });
+
+  it("should change input value when user writes fields", () => {
+    render(<App />);
+    const input = screen.getByLabelText("firstName").nextElementSibling;
+    fireEvent.change(input, { target: { value: "Yoram" } });
+
+    expect(input.value).toBe("Yoram");
+  });
+
+  it("should not send form if every fields are not writes", () => {
+    render(<App />);
+    const onSubmit = jest.fn();
+    const form = screen.getByTestId("form_login").firstChild;
+
+    fireEvent.click(screen.getByTestId("form_login").firstChild);
+    expect(onSubmit).toHaveBeenCalledTimes(0);
+  });
 });
